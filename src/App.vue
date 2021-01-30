8f754575d2281f5f41e8d8fc09dfa14299ffa390
prashanth <prashanth1krishnamurthy@gmail.com>
2021-01-30 15:12:58 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Teleport></Teleport>
+
+  <img alt="Vue logo" src="./assets/logo.png" style="height: 100px" />
+</template>
+
+<script setup>
+import Teleport from "./components/Teleport.vue";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
